47a24bd4dcc782b26a1446095b16178ea232a820
Egbert-518 <1851498@tongji.edu.cn>
2024-12-24 13:27:20 +0800

    add pointops-gaussianformerV2

---
+++ pointops/build/lib.linux-x86_64-3.8/pointops/__init__.py
@@ -0,0 +1,10 @@
+from .pointops import (
+    furthestsampling,
+    grouping,
+    knnquery,
+    subtraction,
+    aggregation,
+    interpolation2,
+
+)
+from .pointops import furthestsampling as farthest_point_sampling
